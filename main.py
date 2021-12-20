07472acdd5aba762c1d9a782ae1d149593631312
Glebfil <gleb.m.filimonov@gmail.com>
2021-12-20 22:06:21 +0300

    Первый образец игры

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
+import time
+import random
 
 def get_points(landmark, shape):
     points = []
@@ -22,39 +23,57 @@ def palm_size2(landmark, shape):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** .5
 
 
-handsDetector = mp.solutions.hands.Hands()
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
+def hand_detector():
+    handsDetector = mp.solutions.hands.Hands()
+    cap = cv2.VideoCapture(0)
+
     ret, frame = cap.read()
-    if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
-        break
+
     flipped = np.fliplr(frame)
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
     results = handsDetector.process(flippedRGB)
     if results.multi_hand_landmarks is not None:
         d = get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
-        cv2.drawContours(flippedRGB, [get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)], 0,
-                         (255, 0, 0), 2)
         (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
         a = np.array([d[0], d[16], d[20]])
         (x2, y2), r2 = cv2.minEnclosingCircle(a)
         ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
         ws2 = palm_size2(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
-        cv2.circle(flippedRGB, (int(x), int(y)), int(r), (0, 0, 255), 2)
-        cv2.circle(flippedRGB, (int(x2), int(y2)), int(r2), (0, 0, 255), 2)
-        print(2 * r / ws)
-        print(2 * r2 / ws2)
-        print("")
         if 2 * r / ws > 1.9 and 2 * r2 / ws2 < 1:
-            cv2.putText(flippedRGB, "Scissors", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=2)
+            return "Scissors"
         elif 2 * r / ws > 1.8:
-            cv2.putText(flippedRGB, "Paper", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=2)
+            return "Paper"
         elif 2 * r / ws < 1.5:
-            cv2.putText(flippedRGB, "Rock", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=2)
+            return "Rock"
+
+    handsDetector.close()
+
+
+print("Greeting Player! Welcome to the Rock-Paper-Scissors game with the computer. To play you should write +:")
+c = input()
+while True:
+    r = random.choice(["Rock", "Paper", "Scissors"])
+    if c == "+":
+        print("Ok, you are ready to play. In a second I will start writing Rock, Paper, Scissors and when I will print SHOOT! you should show your sign.")
+        time.sleep(7)
+        print("Rock")
+        time.sleep(2)
+        print("Paper")
+        time.sleep(2)
+        print("Scissors")
+        time.sleep(2)
+        print("SHOOT!")
+        h = hand_detector()
+        print(h)
+        if r == h:
+            print("Draw!")
+        elif (r == "Rock" and h == "Scissors") or (r == "Scissors" and h == "Paper") or (r == "Paper" and h == "Rock"):
+            print("I won! I showed ", r)
         else:
-            cv2.putText(flippedRGB, "Error", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=2)
-
-    res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
-    cv2.imshow("Hands", res_image)
-
-handsDetector.close()
+            print("Congratulations! You are the winner. I showed", r)
+    else:
+        break
+    time.sleep(3)
+    print("Would you like to play again? Write + or - :")
+    c = input()
+print("Game over!")
